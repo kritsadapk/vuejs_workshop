ea453f0f8501366209c55723eb4d5376accc6713
kritsadapk <kritsada@hospital-os.com>
2017-10-04 17:55:49 +0700

    state

--- timesheet-frontend/src/main.js
+++ timesheet-frontend/src/main.js
@@ -1,6 +1,10 @@
 // เริ่มต้นของโปรแกรม
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import App from './App'
 import './filter'
@@ -9,7 +13,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
-
+Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
